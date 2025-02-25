ec52087343b1000a7b55c71e88da7cb89c4ce511
Leandro Machado <leandrsxe@icloud.com>
2025-02-24 21:30:39 -0300

    Update changes

---
+++ alura-books/src/componentes/Logo/index.js
@@ -0,0 +1,13 @@
+import './estilo.css'
+import logo from '../../imagens/logo.svg'
+
+function Logo () {
+  return (
+    <div className='logo'>
+        <img src={logo} alt= 'logo da Alura Books'></img>
+        <p><strong>Alura</strong>Books</p>
+      </div>
+  )
+}
+
+export default Logo
