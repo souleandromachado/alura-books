2c716103f7a57e4f7d63222c7dea49d00140b535
Leandro Machado <leandrsxe@icloud.com>
2025-03-05 09:53:05 -0300

    Removido os css e adicionado o Style components

--- alura-books/src/componentes/IconesHeader/index.js
+++ alura-books/src/componentes/IconesHeader/index.js
@@ -1,16 +1,25 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`
+const Icones = styled.ul`
+  display: flex;
+  align-items: center;
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
   return(
-    <ul className='icones'>
+    <Icones>
       {icones.map( (icone) => (
-      <li className='icone'><img src={icone} alt='Icones'></img></li>
+      <Icone><img src={icone} alt='Icones'></img></Icone>
     ))}
-    </ul>
+    </Icones>
   )
 }
 
